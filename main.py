694a6d61cba6b185e9893e3a77233cca5179e0ef
BaDelhommer <badelhommer@gmail.com>
2024-05-15 21:39:10 -0500

    cleaned up logic

--- main.py
+++ main.py
@@ -4,26 +4,35 @@ def create_generate_file():
     root_dir = input("Input the relative path to dir containing resources: ")
     generate_file_path = input("Input the relative path for generate.go: ")
 
-
     file_exists = os.path.isfile(generate_file_path)
+    initial_content = ""
+    package_written = False
+    generate_commands_written = False
+
+    if file_exists:
+        with open(generate_file_path, "r") as generate_file:
+            initial_content = generate_file.read()
+            package_written = "package main" in initial_content
+            generate_commands_written = "//go:generate" in initial_content
 
     with open(generate_file_path, "a") as generate_file:
-        command_option = ""
-        if not file_exists:
-            generate_file.write("package main\n\n")
 
+        if not package_written:
+            generate_file.write("package main \n\n")
         
         for subdir, _, files in os.walk(root_dir):
             for file in files:
-                generate_file.seek(0, os.SEEK_END)
-                file_size = generate_file.tell()
-
-                if file_size > 20:
-                    command_option = "bundled.go -append"
-                else: command_option = "bundled.go"
 
                 relative_path = os.path.relpath(os.path.join(subdir, file), start=os.path.dirname(generate_file_path))
                 relative_path = relative_path.replace("\\", "/")
+
+                if generate_commands_written:
+                    command_option = "bundled.go -append"
+                else:
+                    command_option = "bundled.go"
+                    generate_commands_written = True
+
+
                 command = f"//go:generate fyne bundle -o {command_option} ./{relative_path}\n"
                 generate_file.write(command)
 
