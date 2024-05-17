2e1e3ff3358795bf71e8a03cd707f9a1bea144d2
BaDelhommer <badelhommer@gmail.com>
2024-05-17 18:26:27 -0500

    improved gui

--- main.py
+++ main.py
@@ -1,79 +1,76 @@
 import os
 import tkinter as tk
-from graphics import Window
 from tkinter import filedialog
 
-current_step = "source"
+class GenerateTool:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("400x300")
+        self.current_step = "source"
+        self.source_directory = ""
+        self.destination_directory = ""
 
-def select_source_directory():
-    global current_step
-    directory_path = filedialog.askdirectory(title="Select source directory")
-    if directory_path:
-        print(f"Source destination: {directory_path}")
-        current_step = "destination"
-        select_destination_directory(directory_path)
+        self.label = tk.Label(self.root, text="Select the source directory containing resources:")
+        self.label.pack(pady=20)
 
-def select_destination_directory(source):
-    global current_step
-    directory_path = filedialog.askdirectory(title="Select destintation directory")
-    if directory_path:
-        rel_path = directory_path + "/generate.go"
-        print(f"Destination directory: {rel_path}")
-        create_generate_file(source, rel_path)
+        self.button = tk.Button(self.root, text="Select Source Directory", command=self.select_source_directory)
+        self.button.pack()
 
-def create_generate_file(source_directory, destination_directory):
-    root_dir = source_directory
-    generate_file_path = destination_directory
+    def select_source_directory(self):
+        self.source_directory = filedialog.askdirectory(initialdir=os.getcwd(), title="Select Source DIrectory")
+        if self.source_directory:
+            print(f"Source Directory: {self.source_directory}")
+            self.current_step = "destintation"
+            self.label.config(text="Select destination directory for generate.go:")
+            self.button.config(text="Select Destination Directory", command=self.select_destination_directory)
 
-    file_exists = os.path.isfile(generate_file_path)
-    initial_content = ""
-    package_written = False
-    generate_commands_written = False
+    def select_destination_directory(self):
+        self.destination_directory = filedialog.askdirectory(initialdir=os.getcwd(), title="Select Destination DIrectory")
+        if self.destination_directory:
+            rel_path = os.path.join(self.destination_directory, "generate.go")
+            print(f"Destination directory{rel_path}")
+            self.create_generate_file(self.source_directory, rel_path)
 
-    if file_exists:
-        with open(generate_file_path, "r") as generate_file:
-            initial_content = generate_file.read()
-            package_written = "package main" in initial_content
-            generate_commands_written = "//go:generate" in initial_content
+    def create_generate_file(self, source_directory, generate_file_path):
+        root_dir = source_directory
 
-    with open(generate_file_path, "a") as generate_file:
+        file_exists = os.path.isfile(generate_file_path)
+        initial_content = ""
+        package_written = False
+        generate_commands_written = False
 
-        if not package_written:
-            generate_file.write("package main \n\n")
-        
-        for subdir, _, files in os.walk(root_dir):
-            for file in files:
+        if file_exists:
+            with open(generate_file_path, "r") as generate_file:
+                initial_content = generate_file.read()
+                package_written = "package main" in initial_content
+                generate_commands_written = "//go:generate" in initial_content
 
-                relative_path = os.path.relpath(os.path.join(subdir, file), start=os.path.dirname(generate_file_path))
-                relative_path = relative_path.replace("\\", "/")
+        with open(generate_file_path, "a") as generate_file:
+            if not package_written:
+                generate_file.write("package main\n\n")
 
-                if generate_commands_written:
-                    command_option = "bundled.go -append"
-                else:
-                    command_option = "bundled.go"
-                    generate_commands_written = True
+            for subdir, _, files in os.walk(root_dir):
+                for file in files:
+                    absolute_path = os.path.join(subdir, file)
+                    relative_path = os.path.relpath(absolute_path, start=root_dir)
+                    relative_path = f"{relative_path.replace(os.sep, '/')}"
 
+                    if generate_commands_written:
+                        command_option = "bundled.go -append"
+                    else:
+                        command_option = "bundled.go"
+                        generate_commands_written = True
 
-                command = f"//go:generate fyne bundle -o {command_option} ./{relative_path}\n"
-                generate_file.write(command)
+                    command = f"//go:generate fyne bundle -o {command_option} ./{relative_path}\n"
+                    generate_file.write(command)
 
-    print(f"generate.go file updated at {generate_file_path}")
+        print(f"generate.go file updated at {generate_file_path}")
+        self.label.config(text="Process Complete!!")
+        self.button.config(text="Done", state=tk.DISABLED)
 
-def main():
-
-    screen_x = 400
-    screen_y = 300
-    win = Window(screen_x, screen_y)
-
-    if current_step == "source":
-        win.create_button(command=select_source_directory)
-    else:
-        win.create_button(command=select_destination_directory)
-
-    win.wait_for_close()
-    # create_generate_file()
+    def run(self):
+        self.root.mainloop()
 
 if __name__ == "__main__":
-    main()
-
-        
+    tool = GenerateTool()
+    tool.run()
